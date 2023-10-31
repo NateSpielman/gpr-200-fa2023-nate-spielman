6f10469a9fb7cb46908349e427de08ae91fe0bd9
Nate Spielman <nate.spielman@mymail.champlain.edu>
2023-10-31 11:05:12 -0400

    Started working on setting up shape generation functions

---
+++ core/ns/procGen.cpp
@@ -0,0 +1,18 @@
+#include "procGen.h"
+
+namespace ns {
+	ew::MeshData createSphere(float radius, int numSegments)
+	{
+
+	}
+
+	ew::MeshData createCylinder(float height, float radius, int numSegments)
+	{
+
+	}
+
+	ew::MeshData createPlane(float width, float height, int subdivisions)
+	{
+
+	}
+}
